434a9f31d060b0bde920898e338e1a7bd235c669
cmg8431 <cmg8431@gmail.com>
2023-08-17 14:33:57 +0900

    feat: lexical 텍스트 에디터 플러그인 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import Editor from "./components/Editor";
+import "./styles.css";
 
 export const App: React.FC = () => {
-  return <>hello</>;
+  return (
+    <>
+      <Editor />
+    </>
+  );
 };
